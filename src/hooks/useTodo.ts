a0f40b85ff56a0022eebd037821a258fe54b6670
maykel <maykel.winchester@hotmail.com>
2024-06-18 20:20:28 -0300

    fix: adjust to use switch

--- src/hooks/useTodo.ts
+++ src/hooks/useTodo.ts
@@ -7,29 +7,44 @@ type Todo = {
 
 type ActionAdd = { type: 'add'; payload: Todo };
 type ActionRemove = { type: 'remove'; payload: number };
-type ActionChange = { type: 'change'; payload: number };
+type ActionUpdate = { type: 'update'; payload: number };
 
-type TodoActions = ActionAdd | ActionRemove | ActionChange;
+type TodoActions = ActionAdd | ActionRemove | ActionUpdate;
 
-const initialState: Todo[] = [];
+type State = {
+  todos: Todo[];
+};
 
-const reducer = (state: typeof initialState, action: TodoActions): Todo[] =>
-  ({
-    add: [...state, action.payload as Todo],
-    remove: state.filter((_, index) => index !== action.payload),
-    change: state.map((todo, todoIndex) => {
-      if (todoIndex === action.payload) {
-        return {
-          ...todo,
-          done: !todo.done,
-        };
-      }
-      return todo;
-    }),
-  }[action.type]);
+const initialState: State = {
+  todos: [],
+};
+
+const reducer = (prevState: State, action: TodoActions): State => {
+  switch (action.type) {
+    case 'add':
+      return {
+        ...prevState,
+        todos: [...prevState.todos, action.payload],
+      };
+    case 'remove':
+      return {
+        ...prevState,
+        todos: prevState.todos.filter((_, index) => index !== action.payload),
+      };
+    case 'update':
+      return {
+        ...prevState,
+        todos: prevState.todos.map((todo, todoIndex) =>
+          todoIndex === action.payload ? { ...todo, done: !todo.done } : todo,
+        ),
+      };
+    default:
+      return prevState;
+  }
+};
 
 export default function useTodo() {
-  const [todos, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const addTodo = useCallback((task: string) => {
     if (!task) {
@@ -53,13 +68,13 @@ export default function useTodo() {
 
   const updateTodo = useCallback((index: number) => {
     dispatch({
-      type: 'change',
+      type: 'update',
       payload: index,
     });
   }, []);
 
   return {
-    todos,
+    todos: state.todos,
     addTodo,
     removeTodo,
     updateTodo,
